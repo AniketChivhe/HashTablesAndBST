66b400d1b1675e1daca500cef9a99ab2ca2daff4
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-14 01:36:17 +0530

    [Aniket] UC2 of BST added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace BST_UC2_AddingMoreElements
+{
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("UC2_Adding More elements in Tree"); 
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);  //56 is root
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            binarySearch.Display();          
+            Console.ReadKey();
+        }
+    }
+}
