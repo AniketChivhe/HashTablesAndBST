bfda24810d0d7ee02fe5a1af07d7ddbcbc071f84
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-14 01:29:47 +0530

    [Aniket] UC1 of Hash table added

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HashTablesAndBST
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***Hashing Problem***");
+            Console.WriteLine("***USE CASE 2-To find frequency of words *** ");
+            Console.WriteLine("***Words :- to be or not to be");
+
+
+            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(6);
+            
+                    string[] words = { "to", "be", "or", "not", "to", "be" };
+                    int count = 1;
+            
+                foreach (string i in words)
+                {
+                    count = myMapNode.CheckHash(i);
+                    if (count > 1)
+                    {
+                        myMapNode.Add(i, count);
+                    }
+                    else
+                    {
+                        myMapNode.Add(i, 1);
+                    }
+                }
+
+                IEnumerable<string> uniqueItems = words.Distinct<string>();
+                foreach (var i in uniqueItems)
+                {
+                    myMapNode.Display(i);
+                }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
+
