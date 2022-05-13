ecf93d3794132f97a51c8743c66313840dcb59f5
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-14 01:35:08 +0530

    [Aniket] UC1 of BST added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BST_UC1_Insertion
+{
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("UC1_Creating Binary Search Tree"); 
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56); //56 is root
+            binarySearch.Insert(30); //inserting 30 to left
+            binarySearch.Insert(70);//inserting 70 to right
+            binarySearch.Display();
+            Console.ReadKey();
+        }
+    }
+}
